09aeaa9f69aba70f609a1a73d83e7d396ee2d97e
escalade <trondah@gmail.com>
2016-04-09 19:12:36 +0200

    update retroarch

--- packages/emulators/retroarch/package.mk
+++ packages/emulators/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="2e11895"
+PKG_VERSION="d818578"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
